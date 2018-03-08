2e768bff0d07e810a7518838abe43ec3eabc43ff
Kathy Kennedy <nomamanomama@yahoo.com>
2018-03-08 12:45:32 -0600

    initialize firebase in logic js

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAnsYirQz49BPtXfKAzdDER4n0T4dtTE8s",
+    authDomain: "rps-multi-player-f545c.firebaseapp.com",
+    databaseURL: "https://rps-multi-player-f545c.firebaseio.com",
+    projectId: "rps-multi-player-f545c",
+    storageBucket: "",
+    messagingSenderId: "882906915418"
+  };
+  firebase.initializeApp(config);
