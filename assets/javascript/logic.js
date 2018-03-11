cf2a85f548e3beb655d7dec3a70662fdbaf57eb3
nomamanomama <33500937+nomamanomama@users.noreply.github.com>
2018-03-11 03:25:36 -0500

    logic that adds 2 player and initiates first turn

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,24 +10,69 @@ var config = {
   firebase.initializeApp(config);
 
 // database
-var database = firebase.database(); 
+var db = firebase.database(); 
+
+// playersRef references a specific location in our database.
+// All players will be stored in this directory.
+var playersRef = db.ref("/players");
+var chatRef = db.ref("/chat");
+var connectionsRef = db.ref("/connections");
+// '.info/connected' is a special location provided by Firebase that is updated
+// every time the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = db.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function (snapshot) {
+
+  // If they are connected..
+  if (snapshot.val()) {
+
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
+
+// When first loaded or when the connections list changes...
+playersRef.on("value", function (snapshot) {
+  if (snapshot.child('1').exists() && snapshot.child('2').exists()) {
+    gameOpen = false;
+    // //update local object player 1 values
+    // p1.name = p1Data.name;
+    // p1.wins = p1Data.wins;
+    // p1.losses = p1Data.losses;
+    // p1.choice = p1Data.choice;
+
+    // //update local object player 2 values
+    // p2.name = p2Data.name;
+    // p2.wins = p2Data.wins;
+    // p2.losses = p2Data.losses;
+    // p2.choice = p2Data.choice;
+  } 
+
+});
+
+// -------------------------------------------------------------- (CRITICAL - BLOCK) 
 
 //initial values
-var player1 = {
+var p1 = {
   choice: "",
   name: "",
   wins: 0,
   losses: 0,
 }
 
-var player2 = {
+var p2 = {
   choice: "",
   name: "",
   wins: 0,
   losses: 0,
 }
 
-var playerIndex;
+var myPlayerIndex;
 
 var turn = 0;
 
@@ -35,140 +80,200 @@ var gameOpen = true;
 
 $(document).ready(function () {
   
-$("#player1-buttons").hide();
-$("#player2-buttons").hide();
+  $("#player1-buttons").hide();
+  $("#player2-buttons").hide();
 
 
-database.ref().on("value", function (snapshot){
-  if(snapshot.child("1").exists() && snapshot.child("2").exists())
-    gameOpen = false;
-});
+  db.ref().on("value", function (snapshot){
+    var data = snapshot.val();
+    console.log(data);
+    //if the database is not NULL
+    if(data){
+ 
+    }
+  });
 
-//function to display Player input field or playermessage
-function displayNewPlayerBox(show, message) {
-  if (show) {
-    $("#newPlayerBox").show();
-    $("#playerMessage").hide();
-  }
-  else {
-    $("#newPlayerBox").hide();
-    $("#playerMessage").show();
-    $("#playerMessage").text(message);
-  }
-} 
+  //function to display Player input field or playermessage
+  function displayNewPlayerBox(show, message) {
+    if (show) {
+      $("#newPlayerBox").show();
+      $("#playerMessage").hide();
+    }
+    else {
+      $("#newPlayerBox").hide();
+      $("#playerMessage").show();
+      $("#playerMessage").text(message);
+    }
+  } 
 
-//on $(#newPlayer) click, assign Player number and display welcome message
-$("#newPlayer").on("click", function () {
-    var playerName = $("#newPlayerName").val().trim();
-    console.log(playerName + " signed in");
-  if (gameOpen && playerName != "") {
-    //if Player 1 exists, this is player 2
-    var ref = firebase.database().ref();
-    ref.once("value")
-      .then(function (snapshot) 
-      {
-        if (snapshot.child('players').exists()) {
-          database.ref('players/' + '2').set({
-            name: playerName,
-            wins: 0,
-            losses: 0,
-            choice: ""
-          });
-          playerIndex = 2;
-        }
-        else {
-          //else this is player 1
-          database.ref('players/' + '1').set({
-            name: playerName,
-            wins: 0,
-            losses: 0,
-            choice: ""
-          });
-          playerIndex = 1;
-        }
-      });
-  }
-  displayNewPlayerBox(false, "Welcome to the game " + playerName + "!");
+  //on $(#newPlayer) click, assign Player number and display welcome message
+  $("#newPlayer").on("click", function () {
+      var playerName = $("#newPlayerName").val().trim();
 
-});
+      var player1Ref = db.ref('/players/1');
+      var player2Ref = db.ref('/players/2');
 
-database.ref('players').on("value", function (snapshot) {
-  //when a child is added update the player display
-  //after both players exist, start the game
-  var ref = firebase.database().ref();
-  ref.once("value")
-    .then(function (snapshot) {
-       if (snapshot.child("1").exists() && snapshot.child("2").exists()) {
-        gameOpen = false;
-        initGame();
-       }
-    });
-});
+      var player1exists = false;
+      console.log(playerName + " signed in");
+    if (gameOpen && playerName != "") {
+      //if playersRef not null check to see if "1" exists
+      //else check to see if "2" exists
+      db.ref('/players').once("value")
+        .then (function (snapshot) {
+          console.log(snapshot.val());
+          console.log(snapshot.child('1').exists());
+          if (snapshot.child('1').exists()) {
+            player2Ref.set({
+              name: playerName,
+              wins: 0,
+              losses: 0,
+              choice: ""
+            },function (error){
+              console.log("Error: " + error.code);
+            });
+            myPlayerIndex = 2;
+            
+          }
+          else{
+            player1Ref.set({
+              name: playerName,
+              wins: 0,
+              losses: 0,
+              choice: ""
+            }, function (error) {
+              console.log("Error: " + error.code);
+            });
+            myPlayerIndex = 1;
+          }
+          displayNewPlayerBox(false, "Welcome to the game " + playerName + "! You're Player " + myPlayerIndex);
+        });
+
+      }
+
+  });
+
+  db.ref('/players').on("value", function (snapshot) {
+    //when a child is added update the player display
+    //after both players exist, start the game
+    console.log("players values changed");
+    if (snapshot.child('1').exists() && snapshot.child('2').exists()){
+      gameOpen = false;
+      initGame();
+    }
+
+  });
+
+  db.ref('/players/1/choice').on("value", function (snapshot) {
+    //when a child is added update the player display
+    //after both players exist, start the game
+    console.log("players 1 made choice - move to player 2 choice");
+    setCurrentPlayerDisplay(2);
+
+  });
 
 // $(document).on("close", function () {
 //   database.ref().onDisconnect();
 // }
 
-//function to remove a player by index from database
-function removePlayer(index) { 
-  database.ref('players/' + index).remove()
-    .then(function() {
-      console.log("removed player " + index);
+  //function to remove a player by index from database
+  function removePlayer(index) { 
+    db.ref('players/' + index).remove()
+      .then(function() {
+        console.log("removed player " + index);
+      })
+      .catch(function (error) {
+        console.log("remove player " + index + " failed: " + error.code);
+      })
+  }
+
+  //create playerDisplay() function for Playerbox content
+  function setCurrentPlayerDisplay(index) {
+
+    if (myPlayerIndex === index)
+    {
+      //set current player name
+      $("#player" + index + "-name").show();
+      //show 3 buttons for: Rock, Paper and Scissors
+      $("#player" + index + "-buttons").show();
+    }
+    else {
+      $("#player" + myPlayerIndex).removeClass("active");
+      //hide 3 buttons for: Rock, Paper and Scissors
+      $("#player" + myPlayerIndex + "-buttons").hide();
+    }
+
+    $("#player" + index).addClass("active");
+
+  }
+
+  function newTurn() {
+    turn++;
+    db.ref('turn').set({
+      turn: turn
+    });
+    //highlight player 1 box
+  }
+
+  function initGame() {
+    //start a new turn
+    newTurn();
+    //display buttons for Player 1 to choose
+    setCurrentPlayerDisplay(1);
+
+  }
+
+  //on $(#newPlayer) click, assign Player number and display welcome message
+  $(".rpsButton").on("click", function () {
+    db.ref('players/' + myPlayerIndex).update({
+      choice: $(this).val()
     })
-    .catch(function (error) {
-      console.log("remove player " + index + " failed: " + error.message);
-    })
-}
-
-//create playerDisplay() function for Playerbox content
-function setPlayerDisplay(index) {
-  //set current player name
-  $("#player" + index + "-name").show();
-  //show 3 buttons for: Rock, Paper and Scissors
-  $("#player" + index + "-buttons").show();
-}
-
-
-function newTurn() {
-  turn++;
-  database.ref().set({
-    turn: turn
+    //if I'm player 2, evaluate the choices for both players
+    if (myPlayerIndex === 2)
+      calculateResult();
+    else if (myPlayerIndex === 1)
+      setCurrentPlayerDisplay(2);  
   });
-  //highlight player 1 box
-}
 
-function initGame() {
-  //start a new turn
-  newTurn();
-  //display buttons for Player 1 to choose
-  setPlayerDisplay(1);
-
-}
-
-database.ref('players/1').on("value", function (snapshot) {
-  player1.name = snapshot.val().name;
-  player1.wins = snapshot.val().wins;
-  player1.losses = snapshot.val().losses;
-  player1.choice = snapshot.val().choice;
-});
-database.ref('players/2').on("value", function (snapshot) {
-  player2.name = snapshot.val().name;
-  player2.wins = snapshot.val().wins;
-  player2.losses = snapshot.val().losses;
-  player2.choice = snapshot.val().choice;
-  calculateResult();
-});
-
-//on $(#newPlayer) click, assign Player number and display welcome message
-$(".rpsButton").on("click", function () {
-  database.ref('players/' + playerIndex).set({
-    choice: $(this).val()
-  })
-});
-
-function calculateResult() {
-  //insert logic for RPS
-}
+  function calculateResult() {
+    //insert logic for RPS
+    var winnerIndex;
+    var ref = db.ref();
+    ref.once("value")
+      .then(function (snapshot) {
+        var player1 = snapshot.child('players/1');
+        var player2 = snapshot.child('players/2');
+        //check for tie game
+        if (player1.choice === player2.choice){
+          $("#result").text("Tie!");
+        }  
+        //test for player 1 wins  
+        else if(player1.choice === "rock" && player2.choice === "scissors"){
+          winnerIndex = 1;  
+          $("#result").text(player1.name + " Wins! Rock beats Scissors!");
+        }
+        else if (player1.choice === "scissors" && player2.choice === "paper"){
+          winnerIndex = 1;
+          $("#result").text(player1.name + " Wins! Scissors cut Paper!");      
+        }
+        else if (player1.choice === "paper" && player2.choice === "rock"){
+          winnerIndex = 1;
+          $("#result").text(player1.name + " Wins! Paper covers Rock!");
+        }
+        //test for player 2 wins
+        else if (player1.choice === "paper" && player2.choice === "scissors"){
+          winnerIndex = 2;
+          $("#result").text(player2.name + " Wins! Scissors cut Paper!");
+        }
+        else if (player1.choice === "scissor" && player2.choice === "rock"){
+          winnerIndex = 2;
+          $("#result").text(player2.name + " Wins! Rock beats Scissors!");
+        }
+        else if (player1.choice === "rock" && player2.choice === "paper"){
+          winnerIndex = 2;
+          $("#result").text(player2.name + " Wins! Paper covers Rock!");
+        }
+      });
+  }    
 
 //handler function for Rock, Paper, Scissor button
 //player1 select first
